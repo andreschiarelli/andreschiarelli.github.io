9fef1994fc821e38746817f4c5a1200a7ae19c40
andreschiarelli <andreschiarelli@gmail.com>
2018-08-05 20:48:00 -0300

    sss
    sss

--- proyecto.js
+++ proyecto.js
@@ -1,29 +1,4 @@
-$(function($){
-	$("form").submit(function(event){
-		event.preventDefault();
 
-		$.ayax({
-			url: "https://formspre.io/andreschiarelli@gmail.com",
-			method:"POST",
-			data: {
-				name: $("#fname").val(),
-				email:$("#email").val(),
-				message:$("#message").val()
-			},
-				dataType: "json"
-			})
-
-				.done(function(){
-				$("#fname").val("");
-				$("#email").val("");
-				$("#message").val("");
-				alert("tu mensaje ha sido enviado!");
-			}).fail(function(){
-				alert("error,intente nuevamente!");
-
-			});
-		});
-});
 
 
 
